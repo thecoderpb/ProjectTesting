d4e0790ecc4d9220a3b4a89d7f969c05cc7e508a
Rashmi Vidushani <rashmividushani2023@gmail.com>
2025-04-09 16:07:08 -0400

    Add email validator class

--- app/src/main/java/com/pratik/rashmi/projecttesting/EmailValidator.kt
+++ app/src/main/java/com/pratik/rashmi/projecttesting/EmailValidator.kt
@@ -9,17 +9,40 @@ import java.util.regex.Pattern
  * An Email format validator for [android.widget.EditText].
  */
 class EmailValidator : TextWatcher {
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        TODO("Not yet implemented")
+
+    internal var isValid = false
+
+    override fun afterTextChanged(editableText: Editable) {
+        isValid = isValidEmail(editableText)
     }
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        TODO("Not yet implemented")
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
+
+    companion object {
+
+        /**
+         * Email validation pattern.
+         */
+        private val EMAIL_PATTERN = Pattern.compile(
+                "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                        "\\@" +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                        "(" +
+                        "\\." +
+                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                        ")+"
+        )
+
+        /**
+         * Validates if the given input is a valid email address.
+         *
+         * @param email    The email to validate.
+         * @return `true`  if the input is a valid email, `false` otherwise.
+         */
+        fun isValidEmail(email: CharSequence?): Boolean {
+            return email != null && EMAIL_PATTERN.matcher(email).matches()
+        }
     }
-
-    override fun afterTextChanged(s: Editable?) {
-        TODO("Not yet implemented")
-    }
-
-
 }
