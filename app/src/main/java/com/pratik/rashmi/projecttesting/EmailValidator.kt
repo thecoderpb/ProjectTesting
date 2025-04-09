59061bb1c916b9ead713420ffbc5b9cf6e3ea312
Pratik B <pratikbyathnal@gmail.com>
2025-04-09 15:37:49 -0400

    Unit testing setup

---
+++ app/src/main/java/com/pratik/rashmi/projecttesting/EmailValidator.kt
@@ -0,0 +1,25 @@
+package com.pratik.rashmi.projecttesting
+
+import android.text.Editable
+import android.text.TextWatcher
+
+import java.util.regex.Pattern
+
+/**
+ * An Email format validator for [android.widget.EditText].
+ */
+class EmailValidator : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
